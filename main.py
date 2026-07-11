a2e4f7cf4c240aaef77125c9754c2204896aec96
Izhar Ul Haq <izhar.pk03@gmail.com>
2026-07-11 20:42:40 +0400

    Upload FinancePro application

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import flet as ft
 
 from database import create_tables
@@ -5,18 +6,18 @@ from pages.login import login_page
 
 
 def main(page: ft.Page):
-
     create_tables()
 
     page.title = "FinancePro"
-
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
-
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
-
 
     login_page(page)
 
 
-
-ft.app(target=main)
+ft.app(
+    target=main,
+    view=ft.AppView.WEB_BROWSER,
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", "10000")),
+)
